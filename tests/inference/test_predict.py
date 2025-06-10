1ffa273f9caf50f41703363f7fa7a630dd2280d8
Sara Miskovich <saramiskovich@gmail.com>
2025-06-10 14:54:34 -0700

    smaller test data, add k2eg test

--- tests/inference/test_predict.py
+++ tests/inference/test_predict.py
@@ -1,7 +1,9 @@
 from typing import Dict, Any
+from unittest import mock
 import traceback
 import pytest
 import torch
+import os
 
 import k2eg
 from inference.predict import Predict
@@ -37,11 +39,18 @@ class TestPredict:
         with pytest.raises(IndexError):  # TODO: add validation to handle empty input
             predictor.predict([])
 
-    def test_writing_predictions_to_k3eg(self, pv_name, labels_value):
-        try:
-            k2eg_client = k2eg.dml("rf-phase-ad", "app-three")
-            k2eg_client.put(f"pva://{pv_name}", labels_value, 5.0)
-        except ValueError:
-            trb = traceback.format_exc()
-            if "[kafka_broker_url] Kafka broker url is mandatory" in str(trb):
-                pytest.skip("k2eg client not available or failed to connect")
+    # Test writing predictions to k2eg
+    def test_writing_predictions_to_k2eg(self, pv_name, labels_value, rootdir):
+        k2eg_conf_dir = f"{rootdir}/../config/"
+        with mock.patch.dict(os.environ, {"K2EG_PYTHON_CONFIGURATION_PATH_FOLDER": k2eg_conf_dir}):
+            try:
+                k2eg_client = k2eg.dml("rf-phase-ad", "app-three")
+                k2eg_client.put(f"pva://{pv_name}", labels_value, 1.0)
+            except ValueError:
+                trb = traceback.format_exc()
+                if "[kafka_broker_url] Kafka broker url is mandatory" in str(trb):
+                    pytest.skip("k2eg client not available or failed to connect")
+            except TimeoutError:
+                trb = traceback.format_exc()
+                if "Function timed out" in str(trb):
+                    pytest.skip("k2eg client timed out")
