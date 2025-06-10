d32b87969bfa561b2fcb126ed271b3afdc34f30f
nstelter-slac <nstelter@slac.stanford.edu>
2025-06-10 09:49:55 -0700

    wip adding buffer thread to process_b

--- process_b.py
+++ process_b.py
@@ -1,9 +1,10 @@
-from multiprocessing import Manager
+from multiprocessing import Manager, Process
 from process import CustomProcessObject
 from mp_logging import default_logging_kwargs, create_worker_logger
 
 from typing import Optional
 
+import time
 
 class ProcessB(CustomProcessObject):
     def __init__(self,
@@ -17,19 +18,47 @@ class ProcessB(CustomProcessObject):
         self.logging_kwargs['logger_name'] = 'process_b'
         self.logger = None
 
+        self.buffer = Manager().list()
+        self.buffer_loader_process = None
+
+    def buffer_loader(self):
+        self.logger.debug("Buffer-loader started")
+        while True:
+            #self.logger.debug("buffer-loader loop iter!!")
+            r = self.queue_one.get() # waits on get unless at timeout arg
+            logger.debug(f"Buffer-loader got: {r}")
+            # don't append here if r's timestamp is older than 5 mins 
+            self.buffer.append(r)
+            if r is None:
+                break
+        logger.debug("Buffer-loader exiting")
+        for handler in logger.handlers:
+            handler.close()
+
     def __call__(self):
         if self.logger is None:
             self.logger = create_worker_logger(**self.logging_kwargs)
-
+            
+        self.loader_process = Process(target=self.buffer_loader)
+        self.loader_process.start()
+        
+        self.logger.debug("ProcessB started")
+        
         while True:
-            if not self.queue_one.empty():
-                r = self.queue_one.get()
-                self.logger.debug(f"ProcessB sees {r}")
-                if r is None or r % 2 == 1:  # enqueue only the odds
-                    self.logger.debug(f"ProcessB enqueuing {r}")
-                    self.queue_two.put(r)
-                if r is None:  # enqueue a None to stop this process
-                    break
+            #self.logger.debug("process-b loop iter!!")
+            # wait for buffer_loader to load some stuff
+            if len(self.buffer) == 0:
+                time.sleep(0.01)
+                continue
 
+            r = self.buffer.pop(0)
+            self.logger.debug(f"ProcessB sees {r}")
+            # process PVs for beam checks, determining window, etc
+            self.queue_two.put(r)
+            if r is None: # enqueue a None to stop this process
+                break
+
+        self.logger.debug("ProcessB exiting")
+        self.loader_process.join()
         for handler in self.logger.handlers:
             handler.close()
