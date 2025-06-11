d3d5d36625d8e8d72cfc08422d3c5376ee820305
Sara Miskovich <saramiskovich@gmail.com>
2025-06-11 10:55:37 -0700

    adjust predict fn to write to pv, add tests/validation for invalid data

--- inference/predict.py
+++ inference/predict.py
@@ -4,7 +4,7 @@ https://github.com/SLAC-ML/CoincAD/blob/phase/core/CoincAD_train.py
 
 import os
 from operator import itemgetter
-from typing import Any, List, Dict
+from typing import Any, Tuple, Dict, List
 
 import torch
 import yaml
@@ -27,17 +27,28 @@ class Predict:
         self.configs = load_configs() or configs
         self.networks = load_models() or networks
 
-    def predict(self, batch: List[torch.Tensor]) -> bool:
+    def predict(
+        self,
+        rf_input_tensor: torch.Tensor,
+        bpm_input_tensor: torch.Tensor,
+        rf_station: str,
+    ) -> bool:
         """Make predictions using the loaded models and provided a single batch of data.
         Args:
-            batch (list): List of input data for the models, should be torch tensors and have a length of 2. The first
-            element should be the RF data with a shape of (D, N), where N is the number of samples and D is the number
-            of RFs (1), and the second element should be the BPM data with a shape of (D, N), where D is the number
-            of BPMs (8).
+            rf_input_tensor (tensor): Tensor of input data for the first model, with a shape of (D, N), where N is
+            the number of samples and D is the number of RF stations (1).
+            bpm_input_tensor (tensor): Tensor of input data for the second model, with a shape of (D, N), where N is
+            the number of samples and D is the number of BPMs (8).
+            rf_station (str): The PV name of the RF station to write the prediction result to K2EG.
         Returns:
             bool: Prediction result, True if an anomaly is detected, False otherwise.
         """
-        return predict(self.configs, self.networks, batch)
+        anomalous = predict_label(
+            self.configs, self.networks, (rf_input_tensor, bpm_input_tensor)
+        )
+        if anomalous:
+            write_prediction_to_k2eg(rf_station)
+        return anomalous
 
 
 def load_models():
@@ -75,15 +86,15 @@ def load_configs():
         return yaml.safe_load(file)
 
 
-def predict(
-    configs: Dict[str, Any], networks: List[TorchModule], batch: List[torch.Tensor]
+def predict_label(
+    configs: Dict[str, Any], networks: List[TorchModule], batch: Tuple[torch.Tensor]
 ) -> bool:
     """Make predictions using the loaded models and provided a single batch of data.
 
     Args:
         configs (dict): Configuration dictionary for the prediction.
         networks (list): List of TorchModule instances representing the models (length should be 2).
-        batch (list): List of input data for the models, should be torch tensors and have a length of 2. The first
+        batch (Tuple): Tuple of input data for the models, should be torch tensors and have a length of 2. The first
         element should be the RF data with a shape of (D, N), where N is the number of samples and D is the number
         of RFs (1), and the second element should be the BPM data with a shape of (D, N), where D is the number
         of BPMs (8).
@@ -127,6 +138,12 @@ def predict(
     return bool(label)
 
 
+def write_prediction_to_k2eg(pv_name: str):
+    # k2eg_client = k2eg.dml("rf-phase-ad", "app-three")
+    # k2eg_client.put(f"pva://{pv_name}", value, timestamp) # this?
+    pass
+
+
 def standardize_tensor(x):
     return (x - torch.mean(x)) / torch.std(x)
 
@@ -135,30 +152,38 @@ def _validate_input(configs, networks, batch):
     """Validate the input batch for the predict function."""
     if len(batch) != 2:
         raise ValueError(
-            "Batch must contain exactly two elements: RF data and BPM data."
+            f"Batch must contain exactly two elements: RF data and BPM data. A length of {len(batch)} was provided."
         )
     if len(networks) != 2:
         raise ValueError(
-            "Networks must contain exactly two TorchModule instances: one for RF and one for BPM."
+            f"Networks must contain exactly two TorchModule instances: one for RF and one for BPM. A length of {len(networks)} was provided."
         )
     if not isinstance(networks[0], TorchModule) or not isinstance(
         networks[1], TorchModule
     ):
-        raise TypeError("Networks must be instances of TorchModule.")
+        raise TypeError(
+            f"Networks must be instances of TorchModule. Types provided: {type(networks[0])}, {type(networks[1])}."
+        )
     if not isinstance(batch[0], torch.Tensor) or not isinstance(batch[1], torch.Tensor):
-        raise TypeError("Batch elements must be torch.Tensor instances.")
+        raise TypeError(
+            f"Batch elements must be torch.Tensor instances. Types provided: {type(batch[0])}, {type(batch[1])}."
+        )
     if batch[0].dim() != 2 or batch[1].dim() != 2:
-        raise ValueError("Batch elements must be 2D tensors (shape: (D, N)).")
+        raise ValueError(
+            f"Batch elements must be 2D tensors (shape: (D, N)). Shape provided: {batch[0].shape}, {batch[1].shape}."
+        )
     if batch[0].shape[0] != 1 or batch[1].shape[0] != 8:
         raise ValueError(
-            "RF data must have shape (1, N) and BPM data must have shape (8, N)."
+            f"RF data must have shape (1, N) and BPM data must have shape (8, N). Shape provided: {batch[0].shape}, {batch[1].shape}."
         )
     if batch[0].shape[1] != batch[1].shape[1]:
         raise ValueError(
-            "RF data and BPM data must have the same number of samples (N)."
+            f"RF data and BPM data must have the same number of samples (N). N provided: {batch[0].shape[1]}, {batch[1].shape[1]}."
         )
     if not isinstance(configs, dict):
-        raise TypeError("Configs must be a dictionary.")
+        raise TypeError(
+            f"Configs must be a dictionary. Type provided: {type(configs)}."
+        )
     if "predict" not in configs or not isinstance(configs["predict"], dict):
         raise ValueError(
             "Configs must contain a 'predict' key with a dictionary value."
@@ -173,4 +198,13 @@ def _validate_input(configs, networks, batch):
     ):
         raise ValueError(
             "Configs['predict'] must contain 'thres', 'exact', 'standardize', 'sigmoid', and 'device' keys."
+        )
+    if batch[0].numel() == 0 or batch[1].numel() == 0:
+        raise ValueError("Input tensors must not be empty.")
+    if torch.isnan(batch[0]).any() or torch.isnan(batch[1]).any():
+        raise ValueError("Input tensors must not contain NaN values.")
+    if batch[0].shape[1] < 1065 or batch[1].shape[1] < 1065:
+        # Will raise "RuntimeError: mat1 and mat2 shapes cannot be multiplied"
+        raise ValueError(
+            f"Input tensors must have at least 1065 samples (N) in the second dimension. Shape provided: {batch[0].shape}, {batch[1].shape}."
         )
--- tests/inference/test_predict.py
+++ tests/inference/test_predict.py
@@ -4,8 +4,10 @@ import traceback
 import os
 
 import pytest
+from torch import zeros
 
 import k2eg
+from k2eg.dml import OperationTimeout
 from inference.predict import Predict
 
 
@@ -18,9 +20,9 @@ class TestPredict:
         predictor = Predict()
 
         # Get data that should return a prediction of True
-        rf_data, bpm_data, pv_names = test_data_set[0]
+        rf_data, bpm_data, pv_name = test_data_set[0]
         # Run prediction
-        predictions = predictor.predict([rf_data, bpm_data])
+        predictions = predictor.predict(rf_data, bpm_data, pv_name)
         # Check configs loaded correctly
         assert predictor.configs == configs
         # Check if predictions are of the expected shape
@@ -29,9 +31,9 @@ class TestPredict:
         assert predictions
 
         # Get data that should return a prediction of False
-        rf_data, bpm_data, pv_names = test_data_set[1]
+        rf_data, bpm_data, pv_name = test_data_set[1]
         # Run prediction
-        predictions = predictor.predict([rf_data, bpm_data])
+        predictions = predictor.predict(rf_data, bpm_data, pv_name)
         # Check configs loaded correctly
         assert predictor.configs == configs
         # Check if predictions are of the expected shape
@@ -39,13 +41,20 @@ class TestPredict:
         # Check if predictions match the expected output data
         assert not predictions
 
-    def test_predict_with_invalid_input(self):
-        pass  # TODO: add validation to handle invalid input
+    def test_predict_with_wrong_type(self):
+        predictor = Predict()
+        with pytest.raises(TypeError):
+            predictor.predict([1], [2], [3])
 
     def test_predict_with_empty_input(self):
         predictor = Predict()
         with pytest.raises(ValueError):
-            predictor.predict([])
+            # add nans to the input tensors
+            in1 = zeros((1, 1066))
+            in2 = zeros((8, 1066))
+            in1[0, 0] = float("nan")
+            in2[0, 0] = float("nan")
+            predictor.predict(in1, in2, "pv")
 
     # Test writing predictions to k2eg
     def test_writing_predictions_to_k2eg(self, pv_name, labels_value, rootdir):
@@ -59,8 +68,17 @@ class TestPredict:
             except ValueError:
                 trb = traceback.format_exc()
                 if "[kafka_broker_url] Kafka broker url is mandatory" in str(trb):
+                    k2eg_client.close()
                     pytest.skip("k2eg client not available or failed to connect")
             except TimeoutError:
                 trb = traceback.format_exc()
                 if "Function timed out" in str(trb):
+                    k2eg_client.close()
                     pytest.skip("k2eg client timed out")
+            except OperationTimeout:
+                trb = traceback.format_exc()
+                if "Timeout during start" in str(trb):
+                    k2eg_client.close()
+                    pytest.skip("k2eg client operation timed out")
+
+            k2eg_client.close()
