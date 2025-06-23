76c1982b224703786f804ecb5219c780f58b30a3
nstelter-slac <nstelter@slac.stanford.edu>
2025-06-22 23:29:33 -0700

    MNT: just have the 'queue_one.get(...)" in the try:except block
    especially since "queue_one.get(...)" is expected to 'except' every time
    there is no data available, we want it to have its own block catching
    just it's exceptions (cleaner, avoids missing other exceptions we decide
    to catch later, etc...)

--- process_b.py
+++ process_b.py
@@ -52,44 +52,42 @@ class ProcessB(CustomProcessObject):
         while True:
             try:
                 r = self.queue_one.get(timeout=0.05) # wait 50ms
-
-                if r is None: # enqueuing a None should stop this process
-                    break
-
-                # need to be sure each iteration of this processing loop is <= 1 second
-                # (new data comes each second from process_a, so data will pile-up if our processing takes over 1 second)
-                start = time.perf_counter()
-
-                # parse the k2eg snapshot and update buffer
-                self.buffer.update(r)
-
-                # process data in buffer and get stuff to pass to process_c and CoAD
-                # valid_windows = self.buffer.get_valid_windows()
-                # result = self.find_candidates()
-
-                end = time.perf_counter()
-                elapsed_ms = (end - start) * 1000
-                self.logger.debug(f"process_b iteration took : {elapsed_ms:.2f} ms")
-                if elapsed_ms > 1000: # have to be <= 1 sec
-                    self.logger.warning(f"process_b iteration is slow!! : {elapsed_ms:.2f} ms")
-
-                
-                # placeholder: dummy data for ProcessC:
-                fake_rf_input_tensor = np.random.rand(1, 1066).astype(np.float32)
-                fake_bpm_input_tensor = np.random.rand(8, 1066).astype(np.float32)
-                fake_pv_name = "fake_pv_name"
-
-                # expand later with any more info we need to send to process_c
-                fake_output_data = {
-                    "timestamp": end,
-                    "rf_input": fake_rf_input_tensor,
-                    "bpm_input": fake_bpm_input_tensor,
-                    "pv_name": fake_pv_name,
-                }
-                self.queue_two.put(fake_output_data)
-
             except Empty:
                 continue
+
+            if r is None: # enqueuing a None should stop this process
+                break
+
+            # need to be sure each iteration of this processing loop is <= 1 second
+            # (new data comes each second from process_a, so data will pile-up if our processing takes over 1 second)
+            start = time.perf_counter()
+
+            # parse the k2eg snapshot and update buffer
+            self.buffer.update(r)
+
+            # process data in buffer and get stuff to pass to process_c and CoAD
+            # valid_windows = self.buffer.get_valid_windows()
+            # result = self.find_candidates()
+
+            end = time.perf_counter()
+            elapsed_ms = (end - start) * 1000
+            self.logger.debug(f"process_b iteration took : {elapsed_ms:.2f} ms")
+            if elapsed_ms > 1000: # have to be <= 1 sec
+                self.logger.warning(f"process_b iteration is slow!! : {elapsed_ms:.2f} ms")
+
+            # placeholder: dummy data for ProcessC:
+            fake_rf_input_tensor = np.random.rand(1, 1066).astype(np.float32)
+            fake_bpm_input_tensor = np.random.rand(8, 1066).astype(np.float32)
+            fake_pv_name = "fake_pv_name"
+
+            # expand later with any more info we need to send to process_c
+            fake_output_data = {
+                "timestamp": end,
+                "rf_input": fake_rf_input_tensor,
+                "bpm_input": fake_bpm_input_tensor,
+                "pv_name": fake_pv_name,
+            }
+            self.queue_two.put(fake_output_data)
 
         self.logger.debug("shutting down process_b")
 
