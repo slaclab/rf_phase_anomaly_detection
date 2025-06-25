d881c3f2b087f6c639c5a443325cecc0e9fb1c69
Finn O'Shea <finn.h.oshea@gmail.com>
2025-06-25 08:42:17 -0700

    compute relative energy from offset

--- scoring.py
+++ scoring.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 
-from beam_check_config import MAD_LENGTH, CONSECUTIVE_LENGTH, X_DISPERSIVE_BPMS, Y_DISPERSIVE_BPMS
+from beam_check_config import MAD_LENGTH, CONSECUTIVE_LENGTH, DISPERSIVE_BPMS
 
 
 def compute_score_1(
@@ -35,15 +35,18 @@ def compute_score_1(
     -------
         np.ndarray of the MAD for the entire array
     """
+    # if bpm_signals contains the dispersive bpms
+    # convert from mm to relative energy
+    for pv_name, disp in DISPERSIVE_BPMS.items():
+        try:
+            time_series = bpm_signals[pv_name]
+        except KeyError:
+            pass
+        else:  # force a local overwrite here
+            bpm_signals[pv_name] = time_series / (disp * 10)
+
     individual_scores = []
     for pv_name, time_series in bpm_signals.items():
-        if pv_name in X_DISPERSIVE_BPMS:
-            disp = X_DISPERSIVE_BPMS[pv_name]
-            time_series = time_series / (disp * 10) #convert mm to % energy
-        elif pv_name in Y_DISPERSIVE_BPMS:
-            disp = Y_DISPERSIVE_BPMS[pv_name]
-            time_series = time_series / (disp * 10)
-        
         # Compute MAD-based score
         median = np.median(
             sliding_window_view(time_series, window_shape=MAD_LENGTH),
