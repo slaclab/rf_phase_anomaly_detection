090c5e5777a7a1c91ae941fc52afd34b0981e025
nstelter <nstelter@slac.stanford.edu>
2025-07-31 22:05:24 -0700

    MNT: log case when snapshot has late data (not possible?)

--- snapshot_fixer.py
+++ snapshot_fixer.py
@@ -74,6 +74,11 @@ class SnapshotFixer:
                     self.temp_storage[pv].popleft()
                     in_window_timestamps.append(ts_ns)
                     in_window_values.append(get_value(entry))
+                elif ts_ns < start_time:
+                    # late data (data that belongs in previous snapshot) should not get sent, log a warning so we will know if it somehow happens
+                    self.logger.warning(f"Snapshot {self.num_snapshots_processed} had a late data-point!")
+                    self.temp_storage[pv].popleft()  # just throw this data-point away for now (handle later if recurring issue)
+                    break
                 else:
                     break  # data expected in future snapshot, leave in queue for later processing
 
