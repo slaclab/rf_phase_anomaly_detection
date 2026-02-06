695aa01bcd869c21894560122831145ce2f9599f
Finn H. O'Shea <foshea@slac.stanford.edu>
2026-02-06 11:20:41 -0800

    updates candidate_saver module to save into sub-directories to manage file counts

--- candidate_saver.py
+++ candidate_saver.py
@@ -31,52 +31,62 @@ class Saver:
         logging_kwargs : dict, optional
             See mp_logging documentation for the set of logging kwargs.
         """
+        self.root_directory = directory
         self.directory = directory
 
         self.logging_kwargs = logging_kwargs
         self.logging_kwargs["logger_name"] = self.logger_name
-        self.logger = None
+        self.logger = create_worker_logger(**self.logging_kwargs)
+        self.logger.info(f"Initializing {str(self)}")
 
         self.file_counter = None
 
-    def initialize(self):
-        if self.directory is not None:
-            self.logger = create_worker_logger(**self.logging_kwargs)
-            self.logger.info("Initializing saver")
-            if os.path.exists(self.directory):
-                self.logger.info(f"Saving to directory {self.directory:s}")
-                self.file_counter = len(
-                    [f for f in os.listdir(self.directory)
-                     if f.startswith(self.keep_prefix) and f.endswith(self.filename_suffix)]
-                )
-            else:
-                self.logger.info(f"Directory {self.directory:s} not found, creating it...")
-                os.makedirs(self.directory, exist_ok=True)
-                self.file_counter = 0
+    def __str__(self):
+        return 'Saver'
+
+    def initialize(self, directory: str) -> None:
+        """
+        This method is meant to be called by self.save to keep self.directory
+        pointing to a daily directory.
+        """
+        if directory == self.directory:
+            # on first run, self.directory = self.root_directory, but directory
+            # will be self.directory/date, so this will pass
+            return
+
+        if os.path.exists(directory):
+            self.logger.info(f"Saving to directory {directory:s}")
+            self.file_counter = len(
+                [f for f in os.listdir(directory)
+                 if f.startswith(self.keep_prefix) and f.endswith(self.filename_suffix)]
+            )
+        else:
+            self.logger.info(f"Directory {directory:s} not found, creating it...")
+            os.makedirs(directory, exist_ok=True)
+            self.file_counter = 0
+
+        self.directory = directory
 
     def save(self, anomaly: dict, reject: bool = False) -> None:
-        if self.file_counter is None:
-            self.initialize()
-        if self.directory is not None:
-            if not reject:  # not reject is an anomaly candidate to keep
-                log_verb = self.keep_verb
-                prefix = self.keep_prefix
-            else:  # reject this anomaly candidate
-                log_verb = self.reject_verb
-                prefix = self.reject_prefix
-
-            self.logger.debug(
-                f"Saving {log_verb:s} number {self.file_counter:d}"
-            )
+        if not reject:  # not reject is an anomaly candidate to keep
+            log_verb = self.keep_verb
+            prefix = self.keep_prefix
+        else:  # reject this anomaly candidate
+            log_verb = self.reject_verb
+            prefix = self.reject_prefix
+        if self.root_directory is not None:
             u = datetime.fromtimestamp(anomaly["candidate_timestamp"] / NANOSECS_IN_1_SEC)  # time in Pacific time
             date = str(u.date()).replace("-", "")
             time = str(u.time()).split('.')[0].replace(":", "")
-            fn = os.path.join(
-                self.directory,
-                f"{prefix:s}_{date:s}_{time:s}_{self.file_counter:06d}{self.filename_suffix:s}"
-            )
-            np.savez_compressed(fn, **anomaly, allow_pickle=True)  # pickle is required to save objects
+            self.initialize(directory=os.path.join(self.root_directory, date))
+
+            self.logger.debug(f"Saving {log_verb:s} number {self.file_counter:d}")
+            fn = f"{prefix:s}_{date:s}_{time:s}_{self.file_counter:06d}{self.filename_suffix:s}"
+            fp = os.path.join(self.directory, fn)
+            np.savez_compressed(fp, **anomaly, allow_pickle=True)  # pickle is required to save objects
             self.file_counter += 1
+        else:
+            self.logger.debug(f"No directory specified, not saving {log_verb:s}")
 
 
 class CandidateSaver(Saver):
@@ -86,6 +96,9 @@ class CandidateSaver(Saver):
     keep_verb = "candidate"
     reject_verb = "reject"
 
+    def __str__(self):
+        return 'CandidateSaver'
+
 
 class AnomalySaver(Saver):
     keep_prefix = "confirmed_anomaly"
@@ -93,3 +106,6 @@ class AnomalySaver(Saver):
     logger_name = "anomaly_saver"
     keep_verb = "anomaly"
     reject_verb = "reject"
+
+    def __str__(self):
+        return 'AnomalySaver'
